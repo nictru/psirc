beb8fb4b2df8541a4f12e8bd72f94b1489fb5014
Nico Trummer <nictru32@gmail.com>
2024-06-16 21:45:46 +0200

    Prevent kstring_t name collision

--- psirc-quant/src/kseq.h
+++ psirc-quant/src/kseq.h
@@ -31,6 +31,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
+#include <htslib/kstring.h>
 
 #define KS_SEP_SPACE 0 // isspace(): \t, \n, \v, \f, \r
 #define KS_SEP_TAB   1 // isspace() && !' '
@@ -77,14 +78,6 @@
 		}													\
 		return (int)ks->buf[ks->begin++];					\
 	}
-
-#ifndef KSTRING_T
-#define KSTRING_T kstring_t
-typedef struct __kstring_t {
-	size_t l, m;
-	char *s;
-} kstring_t;
-#endif
 
 #ifndef kroundup32
 #define kroundup32(x) (--(x), (x)|=(x)>>1, (x)|=(x)>>2, (x)|=(x)>>4, (x)|=(x)>>8, (x)|=(x)>>16, ++(x))
